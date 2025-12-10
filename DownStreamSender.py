d41bea040b41b3be95aaaaf9df6e238dad3a12a1
Tony Wang <t85wang@uwaterloo.ca>
2025-12-10 10:57:52 -0500

    changed code generator

--- DownStreamSender.py
+++ DownStreamSender.py
@@ -8,7 +8,7 @@ def send_payload(subject, sender, current_user, html, timestamp):
     dt = datetime.fromtimestamp(int(timestamp) / 1000, tz=timezone.utc)
     iso_date = dt.isoformat().replace("+00:00", "Z")
 
-    code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(CODE_LENGTH))
+    code = ''.join(random.choice(string.ascii_uppercase) for _ in range(CODE_LENGTH))
     
     formatted_subject = f"<<{current_user}>>||{code}|| {subject}"
     
@@ -17,6 +17,8 @@ def send_payload(subject, sender, current_user, html, timestamp):
         "date": iso_date,
         "textAsHTML": html,
     }
+
+    print(payload)
 
     url = "https://clients-shared-101.helixautomation.dev/webhook/garde-robe/process_email"
     response = requests.post(url, json=payload)
