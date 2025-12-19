4f14aa9773be0b08524345f1aa66c553abe2667e
Tony Wang <t85wang@uwaterloo.ca>
2025-12-18 19:49:54 -0500

    add timestamp in log

--- GmailFilter.py
+++ GmailFilter.py
@@ -3,7 +3,11 @@ import uvicorn
 import threading
 from TaskScheduler import TaskScheduler
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 def start_fastapi():
     uvicorn.run(
