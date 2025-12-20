a54dca174b8dc77eac6c8c5f43e7c2acd6ad07bf
Tony Wang <t85wang@uwaterloo.ca>
2025-12-19 21:00:18 -0500

    modified according to downstream protocol

--- DownStreamSender.py
+++ DownStreamSender.py
@@ -1,9 +1,15 @@
+import json
+import time
 import random
 import string
 import requests
 from datetime import datetime, timezone
 
-def send_payload(subject, sender, current_user, html, timestamp):
+with open("config.json", "r") as f:
+    config = json.load(f)
+url = config["downstreamEndpoint"]
+
+def send_payload(subject, sender, current_user, html, text, timestamp):
     CODE_LENGTH = 6
     dt = datetime.fromtimestamp(int(timestamp) / 1000, tz=timezone.utc)
     iso_date = dt.isoformat().replace("+00:00", "Z")
@@ -12,13 +18,25 @@ def send_payload(subject, sender, current_user, html, timestamp):
     
     formatted_subject = f"<<{current_user}>>||{code}|| {subject}"
     
-    payload = {
-        "subject": formatted_subject,
-        "date": iso_date,
-        "textAsHTML": html,
-    }
-
-    url = "https://clients-shared-101.helixautomation.dev/webhook/garde-robe/process_email"
+    payload = [
+        {
+            "email": {
+                "html": html,
+                "text": text,
+                "textAsHtml": html,
+                "subject": formatted_subject,
+                "date": iso_date,
+            }
+        }
+    ]
+    
     response = requests.post(url, json=payload)
+
+    '''
+    with open(f"payload{timestamp}-{int(time.time())}.json", "w") as f:
+        json.dump(payload, f, indent=4)
+    with open(f"payload{timestamp}-{int(time.time())}.html", "w") as f:
+        f.write(html)
+    '''
 
     return response.status_code, response.text
